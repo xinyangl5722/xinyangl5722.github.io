3d7d82d1f0d69b557a59876533c228e882cacb91
xinyangl5722 <146861839+xinyangl5722@users.noreply.github.com>
2023-10-03 11:54:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Xin Yang"; // update the part BETWEEN the "quotes"
+let userName = "xinyangl5722"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
